49fec3b4e319a0f94fdf6c0021f44b09fe3994ba
Ir23 <a645959515@gmail.com>
2025-04-04 20:43:32 +0800

    minimum package

--- local.py
+++ local.py
@@ -1,38 +1,26 @@
-from app import generateHTMLList, AreaId, asyncio, parseList, StreamerList, isRoomIdStream, getHTMLList, parsePage, \
-    UidList
-
-areaList = AreaId
-Depth = 200
-
-
-def printInfoList(info):
-    for inf in info:
-        print(inf)
-
+from app import TimeFormation, UidList, kv, requests, hyperlink
+from datetime import datetime
 
 if __name__ == "__main__":
-    LiveList = []
-    ReplayList = []
-    Type = "并行"
-    if Type == "并行":
-        HTMLList = generateHTMLList(areaList, Depth)
-        asyncio.run(parseList(LiveList, HTMLList))
-    elif Type == "穷举":
-        for streamer in StreamerList:
-            try:
-                isRoomIdStream(streamer, LiveList, ReplayList)
-            except:
-                print("ERROR")
+    url = 'https://api.live.bilibili.com/room/v1/Room/get_status_info_by_uids'
+    params = {
+        'uids[]': UidList,
+    }
+    response = requests.get(url, params=params, headers=kv)
+    current_time = datetime.now()
+    display_time = current_time.strftime("%H:%M:%S")
+
+    if response.status_code == 200:
+        json_data = response.json()['data']
+        print(f"\n于 {display_time} 生成完成\n")
+        for uid in UidList:
+            if uid in json_data and json_data[uid]['live_status'] == 1:
+                name, title, time, address = [json_data[uid][key] for key in
+                                              ['uname', 'title', 'live_time', 'room_id']]
+                given_time = datetime.fromtimestamp(time)
+                time_difference = TimeFormation(current_time - given_time)
+
+                print([name, title, time_difference, hyperlink + str(address)])
+
     else:
-        HTMLList = generateHTMLList(areaList, Depth)
-        for ind in range(len(areaList)):
-            j = 0
-            while True:
-                url = HTMLList[j + ind * Depth]
-                html = getHTMLList(url)
-                if 0 < len(html) < 100:
-                    break
-                else:
-                    j += 1
-                parsePage(html, LiveList, UidList)
-    printInfoList(LiveList)
+        print(f"于 {display_time} 生成失败，错误代码 {response.status_code}，请检查网络或者进行下一次生成。")
